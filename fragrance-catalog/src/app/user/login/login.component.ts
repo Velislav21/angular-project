20ff7943513165ed79a11cab636c38b7667f99e7
Velislav21 <zirovcho34@gmail.com>
2024-11-26 21:21:14 +0200

    .
    added forms module and basic input validation

--- fragrance-catalog/src/app/user/login/login.component.ts
+++ fragrance-catalog/src/app/user/login/login.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
 
+  @ViewChild('loginForm')  form: NgForm | undefined;
+
+  formHandler() {
+    console.log(this.form)
+    console.log('submited')
+  }
 }
